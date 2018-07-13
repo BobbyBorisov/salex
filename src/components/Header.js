9060c67c244571ee99d114f737e5800fbe508405
Bobby Borisov <borislav.borisov.tues@gmail.com>
2018-07-13 10:19:48 +0300

    showing account balance

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Menu} from 'semantic-ui-react';
 import {Link} from "react-router-dom";
+import web3 from '../ethereum/web3';
 
-export default (props) => {
-  return (
-    <Menu style={{marginTop:'10px'}}>
-        <Link to="/"><a className="item">SaleX</a></Link>
+class Header extends Component {
+  state = {
+    balance:''
+  }
 
-      <Menu.Menu position="right">
-        <Link to="/sales"><a className="item">View Sales</a></Link>
-        <Link to="/new"><a className="item">Add New</a></Link>
-        <a className="item">Account: {props.currentAccount}</a>
-      </Menu.Menu>
-    </Menu>
-  )
+  async componentDidUpdate(prevProps){
+    console.log('component did update');
+    if (prevProps.currentAccount !== this.props.currentAccount){
+      console.log('new account '+this.props.currentAccount+'. getting balance');
+      const balance = await web3.eth.getBalance(this.props.currentAccount);
+      this.setState({balance});
+    }
+  }
+  render(){
+    return (
+      <Menu style={{marginTop:'10px'}}>
+          <Link to="/"><a className="item">SaleX</a></Link>
+
+        <Menu.Menu position="right">
+          <Link to="/sales"><a className="item">View Sales</a></Link>
+          <Link to="/new"><a className="item">Add New</a></Link>
+          <a className="item">Account: {this.props.currentAccount} - Balance {parseFloat(web3.utils.fromWei(this.state.balance,'ether')).toFixed(4)} ETH</a>
+        </Menu.Menu>
+      </Menu>
+    )
+  }
 };
+
+export default Header;
